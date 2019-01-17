f1fd14314f83956b18e9b0f01f6ed51c1a8f8c48
Unknown <coreyog@gmail.com>
2019-01-16 20:15:04 -0700

    All code files point to this new repo

--- spew/format_test.go
+++ spew/format_test.go
@@ -72,7 +72,7 @@ import (
 	"testing"
 	"unsafe"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/coreyog/go-spew/spew"
 )
 
 // formatterTest is used to describe a test to be performed against NewFormatter.
