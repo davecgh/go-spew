88d2329e9aa89c4100c8f22ff6c2c26a6d4d060e
klaxxon <jon@gettys.org>
2020-01-19 11:35:11 -0600

    Added ability to ignore fields by name or type.

--- spew/config.go
+++ spew/config.go
@@ -98,11 +98,15 @@ type ConfigState struct {
 	// be spewed to strings and sorted by those strings.  This is only
 	// considered if SortKeys is true.
 	SpewKeys bool
+
+	// Name of fields to ignore
+	ignoreFieldByName map[string]bool
+	ignoreFieldByType map[string]bool
 }
 
 // Config is the active configuration of the top-level functions.
 // The configuration can be changed by modifying the contents of spew.Config.
-var Config = ConfigState{Indent: " "}
+var Config = ConfigState{Indent: " ", ignoreFieldByName: make(map[string]bool), ignoreFieldByType: make(map[string]bool)}
 
 // Errorf is a wrapper for fmt.Errorf that treats each argument as if it were
 // passed with a Formatter interface returned by c.NewFormatter.  It returns
